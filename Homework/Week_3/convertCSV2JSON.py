47dadb60c53b6f9a79bf09a326bbb114d50c2d47
Xandra Vos <vosxandra@gmail.com>
2018-11-20 13:36:35 +0100

    comments added

--- Homework/Week_3/convertCSV2JSON.py
+++ Homework/Week_3/convertCSV2JSON.py
@@ -1,3 +1,10 @@
+# Name: Xandra Vos
+# Student number: 10731148
+#
+# Data Processing
+# Week 3
+# convertCSV2JSON.py
+
 import csv
 import pandas as pd
 import json
@@ -5,10 +12,16 @@ import json
 INPUT_CSV = "afvalbakken.csv"
 
 def load_csv(filename):
+    """
+    Loads csv file into python.
+    """
     file = pd.read_csv(filename)
     return file
 
 def make_json(filename):
+    """
+    Make a JSON file.
+    """
     file.to_json("afvalbakken.json", orient="records")
 
 if __name__ == "__main__":
