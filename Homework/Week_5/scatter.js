48be0cb2d872491103d5aa2321126367317f73f6
Xandra Vos <vosxandra@gmail.com>
2018-11-28 15:17:00 +0100

    start week 5

---
+++ Homework/Week_5/scatter.js
@@ -0,0 +1,12 @@
+/*
+Name: Xandra Vos
+Studentnumber: 10731148
+
+Data Processing - Week 5
+Script to draw a scatterplot onto the scatter.html page.
+*/
+
+window.onload = function() {
+
+  console.log('Yes, you can!')
+};
